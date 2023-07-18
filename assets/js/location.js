c2563d805917e54681e25d878c3b48517eaaff75
Stygian Tanuki <136685351+StygianTanuki@users.noreply.github.com>
2023-07-18 14:04:23 -0400

    map functions on webpage

--- assets/js/location.js
+++ assets/js/location.js
@@ -16,93 +16,89 @@ function search() {
   console.log(content);
 }
 
-async function getCoordinates(param) {
-  // DONT FORGET TO ADD HTTPS WHEN DEPLOYING
-  var apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${param}&limit=5&appid=${apiKey}&units=imperial`;
-  await fetch(apiUrl)
-    .then(function (Response) {
-      return Response.json();
-    })
-    .then(function (data) {
-      var currentTemp = document.querySelector("#currentTemp");
-      var icon = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+// async function getCoordinates(param) {
+//   // DONT FORGET TO ADD HTTPS WHEN DEPLOYING
+//   var apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${param}&limit=5&appid=${apiKey}&units=imperial`;
+//   await fetch(apiUrl)
+//     .then(function (Response) {
+//       return Response.json();
+//     })
+//     .then(function (data) {
+//       var currentTemp = document.querySelector("#currentTemp");
+//       var icon = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
 
-      currentTemp.innerHTML = `<h2>${data.name} <br>Current Forecast:</h2><img src=${icon}></img><div>Tempature: ${data.main.temp}</div><div>Humidty: ${data.main.humidity}</div><div>Wind Speed: ${data.wind.speed} mph</div>`;
+//       currentTemp.innerHTML = `<h2>${data.name} <br>Current Forecast:</h2><img src=${icon}></img><div>Tempature: ${data.main.temp}</div><div>Humidty: ${data.main.humidity}</div><div>Wind Speed: ${data.wind.speed} mph</div>`;
 
-      fiveDay(data.coord.lon, data.coord.lat);
-      citySave(data.name);
-    });
-}
+//       fiveDay(data.coord.lon, data.coord.lat);
+//       citySave(data.name);
+//     });
+// }
 
-async function fiveDay(lon, lat) {
-  var apiFiveDay = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
+// async function fiveDay(lon, lat) {
+//   var apiFiveDay = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
 
-  await fetch(apiFiveDay)
-    .then(function (Response) {
-      return Response.json();
-    })
-    .then(function (data) {
-      // this will clear the existing forecast
-      document.getElementById("forecast").innerHTML = "Five Day Forecast:";
+//   await fetch(apiFiveDay)
+//     .then(function (Response) {
+//       return Response.json();
+//     })
+//     .then(function (data) {
+//       // this will clear the existing forecast
+//       document.getElementById("forecast").innerHTML = "Five Day Forecast:";
 
-      for (var i = 3; i < data.list.length; i += 8) {
-        var icon = `https://openweathermap.org/img/w/${data.list[i].weather[0].icon}.png`;
+//       for (var i = 3; i < data.list.length; i += 8) {
+//         var icon = `https://openweathermap.org/img/w/${data.list[i].weather[0].icon}.png`;
 
-        document.getElementById(
-          "forecast"
-        ).innerHTML += `<div class = "card col-2">
-        <img src=${icon} class = "iconClass"></img>
-        <div>Tempature: ${data.list[i].main.temp}</div>
-        <div>Humidty: ${data.list[i].main.humidity}</div>
-        <div>Wind Speed: ${data.list[i].wind.speed} mph</div>
-        </div>`;
-      }
-    });
-}
+//         document.getElementById(
+//           "forecast"
+//         ).innerHTML += `<div class = "card col-2">
+//         <img src=${icon} class = "iconClass"></img>
+//         <div>Tempature: ${data.list[i].main.temp}</div>
+//         <div>Humidty: ${data.list[i].main.humidity}</div>
+//         <div>Wind Speed: ${data.list[i].wind.speed} mph</div>
+//         </div>`;
+//       }
+//     });
+// }
 
 submitButton.addEventListener("click", function () {
   console.log("click");
-  getCoordinates(citySearch.value);
-  localStorage.setItem("searchCity", JSON.stringify(citySearch));
+  // getCoordinates(citySearch.value);
+  localStorage.setItem("searchCity", JSON.stringify(searchCity));
 });
 // v Google Maps JS integration v
-// Initialize and add the map
-// let map;
 
-// async function initMap() {
-//   // The location of Orlando
-//   const position = { lat: 28.538336, lng: -81.379234 };
-//   // Request needed libraries.
-//   //@ts-ignore
-//   const { Map } = await google.maps.importLibrary("maps");
-//   const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+// document.querySelector("#location") function() {
+//   $("#submitButton").on("click", function() {
+//     // Get the user input from the input field
+//     var userInput = $("#searchCity").val();
 
-//   // The map, centered at Orlando
-//   map = new Map(document.getElementById("map"), {
-//     zoom: 4,
-//     center: position,
-//     mapId: "DEMO_MAP_ID",
+//     // Convert spaces in the user input to plus signs (+) as required by the Google Maps API
+//     var query = userInput.replace(/\s+/g, "+");
+
+//     // Construct the new URL with the updated location query
+//     var newSrc = "https://www.google.com/maps/embed/v1/place?key=AIzaSyAiKNEaOOEcNyMz_CmFsiM5pH9EtvAK5uk&q=" + query;
+
+//     // Update the src attribute of the iframe
+//     $("#mapFrame").attr("src", newSrc);
 //   });
+// };
 
-//   // The marker, positioned at Orlando
-//   const marker = new AdvancedMarkerElement({
-//     map: map,
-//     position: position,
-//     title: "Orlando",
-//   });
-// }
+function mapDisplay() {
+  submitButton.on("click", function () {
+    // Get the user input from the input field
+    var userInput = $("#searchCity").val();
 
-// initMap();
+    // Convert spaces in the user input to plus signs (+) as required by the Google Maps API
+    var query = userInput.replace(/\s+/g, "+");
 
-// function initMap() {
-//   // Create a new map instance
-//   const map = new google.maps.Map(document.getElementById("map"), {
-//     center: { lat: 40.7128, lng: -74.0060 }, // Coordinates for the map center (New York City)
-//     zoom: 12, // Initial zoom level
-//   });
-// }
+    // Construct the new URL with the updated location query
+    var newSrc =
+      "https://www.google.com/maps/embed/v1/place?key=AIzaSyAiKNEaOOEcNyMz_CmFsiM5pH9EtvAK5uk&q=" +
+      query;
 
-// // Trying to get the string from the homepage to this page.
-// if (JSON.parse(localStorage.getItem("enteredCity")) == -1) {
-//   form.style.display = "none";
-// }
+    // Update the src attribute of the iframe
+    $("#mapFrame").attr("src", newSrc);
+
+    mapDisplay();
+  });
+}
