c3fc0ea553b8a92b5ea02dad4c2650e36b449ef4
Justin Moore <justinmoore@Justins-MacBook-Air.local>
2023-07-19 10:20:32 -0400

    made homepage button link to local storage to get city and weather

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,12 +1,19 @@
-
-
 // This part of the code is to allow the homepage to switch to
-// the location.html. Working on getting the city to 
+// the location.html. Working on getting the city to
 // save when switching to the next page.
-document.addEventListener("DOMContentLoaded", function() {
-    var wuffEl = document.getElementById("buttonWuff");
-    wuffEl.addEventListener("click", function() {
-        // localStorage.setItem("enteredCity")
-        window.location.href = "location.html";
-    });
+// document.addEventListener("DOMContentLoaded", function() {
+//     var wuffEl = document.getElementById("buttonWuff");
+//     wuffEl.addEventListener("click", function() {
+//         // localStorage.setItem("enteredCity")
+//         window.location.href = "location.html";
+//     });
+// });
+
+document.addEventListener("DOMContentLoaded", function () {
+  var wuffEl = document.getElementById("buttonWuff");
+  wuffEl.addEventListener("click", function () {
+    var enteredCity = document.querySelector("input[type='text']").value;
+    localStorage.setItem("enteredCity", enteredCity);
+    window.location.href = "location.html";
+  });
 });
